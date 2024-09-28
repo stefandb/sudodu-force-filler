0ea48e20dbd689dc22feece439e4c124ae758cb1
Stefan de Bruin <stefan@procademy.nl>
2024-09-28 14:55:09 +0200

    Add ehader component

---
+++ src/Components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex h-full">
+        <div class=" my-auto font-edu-dots font-semibold text-2xl">
+            Sudoku FORCE Filler
+        </div>
+        <div class="grow">
+        </div>
+        <div class=" my-auto">
+            <button>
+                <cog-icon class="size-12 text-black"/>
+            </button>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { CogIcon } from '@heroicons/vue/24/outline'
+</script>
